f416004876a608a06815c9558ee473f21adcd871
yan.pearson <yan.pearson@gmail.com>
2017-05-10 00:32:46 -0400

    Saving Stores and using mixins

--- dang-thats-delicious/routes/index.js
+++ dang-thats-delicious/routes/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 const storeController = require('../controllers/storeController');
 
-// Do work here
 router.get('/', storeController.homePage);
+router.get('/add', storeController.addStore);
+router.post('/add', storeController.createStore);
 
 module.exports = router;
