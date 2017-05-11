7b87ddd517df2d604fde9a1d9305b92d661b353a
yan.pearson <yan.pearson@gmail.com>
2017-05-10 23:39:52 -0400

    Flash Messsages

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -3,14 +3,14 @@ const Store = mongoose.model('Store');
 
 exports.homePage = (req, res) => {
 	res.render('index');
-}; 
+};
 
 exports.addStore = (req, res) => {
 	res.render('editStore', { title: 'Add Store' });
 };
 
 exports.createStore = async (req, res) => {
-	const store = new Store(req.body);
-	await store.save();
-	res.redirect('/');
+	const store = await (new Store(req.body)).save();
+	req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+	res.redirect(`/store/${store.slug}`);
 };
