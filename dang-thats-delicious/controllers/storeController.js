0daad02744aa51c264001b46f537056ea12dd574
yan.pearson <yan.pearson@gmail.com>
2017-07-05 22:26:58 -0400

    Routing and Templating Single Stores

--- dang-thats-delicious/controllers/storeController.js
+++ dang-thats-delicious/controllers/storeController.js
@@ -62,3 +62,9 @@ exports.updateStore = async (req, res) => {
 	req.flash('success', `Successfully updated <strong>${store.name}</strong>. <a href="/store/${store.slug}">View Store -></a>`);
 	res.redirect(`/stores/${store._id}/edit`);
 };
+
+exports.getStoreBySlug = async (req, res, next) => {
+	const store = await Store.findOne({ slug: req.params.slug });
+	if (!store) return next();
+	res.render('store', { store, title: store.name });
+};
